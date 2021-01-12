c11b8c2e22d2a3989ca13ea232f86d8dbb0acd45
Lili Fuentes <lile7886@gmail.com>
2021-01-12 15:04:26 -0500

    added exercises

--- Anagrams.js
+++ Anagrams.js
@@ -12,3 +12,23 @@
 // For "east", there should be 24 words.
 
 
+const anagrams = (input) => {
+    if (input.length === 1) return [input];
+    let resultArray = [];
+    let inputArray = input.split("");
+    for (let i = 0; i < inputArray.length; i++){
+        const anagramResults = anagrams(spliceNoMutate(inputArray, i).join(""));
+        for (let j = 0; j < anagramResults.length; j++){
+            resultArray.push(inputArray[i] + anagramResults[j]);
+        }
+    }
+    return resultArray;
+}
+
+const spliceNoMutate = (input, index) => {
+    let result = [...input];
+    result.splice(index, 1);
+    return result; 
+}
+
+console.log(anagrams("subject"))
